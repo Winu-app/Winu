d7278438822f8b21d715a659bc6fcabf917f0e52
subhash131 <subhashnayak131@gmail.com>
2024-11-16 03:19:18 +0530

    solana contract initial setup

---
+++ contract/programs/stable-fun/src/constants.rs
@@ -0,0 +1,5 @@
+pub const GAME_SEED: &str = "game";
+pub const USER_SEED: &str = "user";
+pub const CLAN_SEED: &str = "clan";
+pub const MASTER_SEED: &str = "master";
+pub const BID_SEED: &str = "bid";
