379419b32290b9a6e0619c3306f752c1988cdec9
subhash131 <subhashnayak131@gmail.com>
2024-11-18 13:52:17 +0530

    add component: my-tournaments

--- frontend/src/actions/tournament/get-tournaments-by-user-id.ts
+++ frontend/src/actions/tournament/get-tournaments-by-user-id.ts
@@ -3,7 +3,7 @@
 export async function getTournamentsByUserId(userId: string) {
   try {
     const domain = process.env.DOMAIN;
-    const res = await fetch(`${domain}/api/tournament/user-id${userId}`, {
+    const res = await fetch(`${domain}/api/tournament/user-id/${userId}`, {
       method: "GET",
     }).then((res) => res.json());
     return res;
--- frontend/src/components/my-tournaments/index.tsx
+++ frontend/src/components/my-tournaments/index.tsx
@@ -17,7 +17,7 @@ const MyTournaments = () => {
       </div>
     );
   }
-
+  console.log(data);
   if (isError) {
     return (
       <div className="w-full h-screen flex items-center justify-center text-white">
@@ -27,17 +27,20 @@ const MyTournaments = () => {
   }
 
   return (
-    <div className="w-[75%] h-full grid [grid-template-columns:repeat(auto-fill,minmax(310px,1fr))] [grid-auto-rows:220px] px-10 py-12 gap-4 overflow-y-scroll custom-scrollbar pb-52">
-      {data?.tournaments?.map((tournament: any) => (
-        <TournamentCard
-          name={tournament.name}
-          host={tournament.host}
-          isActive={tournament.isActive}
-          image={tournament.image}
-          _id={tournament._id}
-          key={tournament._id}
-        />
-      ))}
+    <div className="size-full">
+      <h1 className="pl-10 pt-10 font-semibold text-2xl">My Tournaments</h1>
+      <div className="w-full h-full grid [grid-template-columns:repeat(auto-fill,minmax(310px,1fr))] [grid-auto-rows:220px] px-10 gap-4 pt-4 overflow-y-scroll custom-scrollbar pb-52">
+        {data?.tournaments?.map((tournament: any) => (
+          <TournamentCard
+            name={tournament.name}
+            host={tournament.host}
+            isActive={tournament.isActive}
+            image={tournament.image}
+            _id={tournament._id}
+            key={tournament._id}
+          />
+        ))}
+      </div>
     </div>
   );
 };
