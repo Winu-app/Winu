7868fa515385d7217c387799bf93aa21eb73454f
subhash131 <subhashnayak131@gmail.com>
2024-11-19 22:57:18 +0530

    add action: createClan

---
+++ frontend/src/actions/clan/create-clan.ts
@@ -0,0 +1,16 @@
+"use server";
+import { Clan } from "src/types";
+
+export async function createClan(clan: Clan) {
+  try {
+    console.log(clan);
+    const domain = process.env.DOMAIN;
+    const res = await fetch(`${domain}/api/clan`, {
+      method: "POST",
+      body: JSON.stringify(clan),
+    }).then(async (res) => await res.json());
+    return res;
+  } catch (e) {
+    return { message: e };
+  }
+}
