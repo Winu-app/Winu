2dd4e3e72bf11057bd9702b8161fd974e35c331c
subhash131 <subhashnayak131@gmail.com>
2024-11-19 23:03:53 +0530

    add form: create clan

--- frontend/src/components/clan/create-clan.tsx
+++ frontend/src/components/clan/create-clan.tsx
@@ -1,30 +1,88 @@
 "use client";
 import React, { useState } from "react";
-import InputField from "../ui/input-field";
+import ClanImage from "./clan-image";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+import { createClan } from "src/actions/clan/create-clan";
+import { getUserWithAuthToken } from "src/actions/user/get-user-with-auth-token";
+import { parseAsString, useQueryState } from "nuqs";
 
 const CreateClan = () => {
   const [name, setName] = useState("");
+  const [uniqueName, setUniqueName] = useState("");
   const [description, setDescription] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const queryClient = useQueryClient();
+  const [loading, setLoading] = useState(false);
+
+  const [_, setClanOption] = useQueryState("clan-option", parseAsString);
+
+  const { data: user, isLoading: userLoading } = useQuery({
+    queryFn: () => getUserWithAuthToken(),
+    queryKey: ["current-user"],
+  });
+
+  const { mutate } = useMutation({
+    mutationFn: createClan,
+    onSuccess: (res) => {
+      toast.info(res.message);
+      queryClient.invalidateQueries({ queryKey: ["my-clan"] });
+      setClanOption("my-clan");
+    },
+    onError: (error) => {
+      setLoading(false);
+      toast.error(error.message);
+    },
+  });
+
   return (
-    <div className="size-full py-3 flex items-center justify-center">
-      <div className="p-4 rounded-lg bg-active w-72 flex flex-col gap-2">
-        <div className="w-full flex items-center">
-          <p className="text-xl ">Create clan</p>
-        </div>
-        <InputField
-          fieldName="Clan Name"
-          value={name}
-          setValue={setName}
+    <div className="size-full py-3 flex items-start justify-start">
+      <div className="h-full w-96 flex justify-center">
+        <ClanImage imageUrl={imageUrl} setImageUrl={setImageUrl} />
+      </div>
+      <div className="px-2 flex flex-col gap-2">
+        <input
+          className="text-xl bg-transparent outline-none"
           placeholder="Name"
+          autoFocus
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
         />
-        <InputField
-          fieldName="Short Description"
+        <input
+          className="bg-transparent outline-none"
+          placeholder="Unique Name"
+          autoFocus
+          value={uniqueName}
+          onChange={(e) => {
+            setUniqueName(e.target.value);
+          }}
+        />
+        <textarea
+          placeholder="description..."
+          className="bg-transparent outline-none border border-active rounded-lg resize-none p-2 w-96 h-28"
           value={description}
-          setValue={setDescription}
-          placeholder="description"
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
         />
-        <button className="active:scale-95 transition-transform px-4 py-1 bg-white rounded-lg text-black font-semibold">
-          Create
+        <button
+          className="px-4 py-2 bg-white rounded-lg text-black font-semibold active:scale-95 transition-transform mt-4 disabled:cursor-not-allowed disabled:scale-100"
+          disabled={loading}
+          onClick={() => {
+            mutate({
+              imageUrl,
+              leader: user.user._id,
+              name,
+              uniqueName,
+              coLeaders: [],
+              description,
+              members: [],
+            });
+          }}
+        >
+          Create Clan
         </button>
       </div>
     </div>
