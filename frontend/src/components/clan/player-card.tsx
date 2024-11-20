5c5a4e420a7ac0ec479c4923855c14e7604205da
subhash131 <subhashnayak131@gmail.com>
2024-11-20 22:24:04 +0530

    update component: PlayerCard

--- frontend/src/components/clan/player-card.tsx
+++ frontend/src/components/clan/player-card.tsx
@@ -1,21 +1,33 @@
 import Image from "next/image";
 import React from "react";
+import Logo from "../ui/logo";
 
-const PlayerCard = () => {
+const PlayerCard = ({
+  name,
+  position,
+  image,
+}: {
+  name: string;
+  position: "Leader" | "Member" | "Co-Leader";
+  image?: string;
+}) => {
   return (
     <div className="size-28 flex items-center justify-center flex-col">
       <div className="size-16 rounded-full bg-active border overflow-hidden">
-        <Image
-          src="https://i.pinimg.com/236x/f5/1d/0a/f51d0a59def3381376fbbb99fb7c4825.jpg"
-          width={1}
-          height={1}
-          className="size-full object-cover"
-          alt="Leader"
-        />
+        {!image && <Logo className="size-full" />}
+        {image && (
+          <Image
+            src={image}
+            width={1}
+            height={1}
+            className="size-full object-cover"
+            alt="Leader"
+          />
+        )}
       </div>
       <div className="px-2 py-0.5 bg-active rounded-lg text-xs text-center">
-        <p>Name</p>
-        <p className="text-[0.6rem]">Leader</p>
+        <p>{name}</p>
+        <p className="text-[0.6rem]">{position}</p>
       </div>
     </div>
   );
