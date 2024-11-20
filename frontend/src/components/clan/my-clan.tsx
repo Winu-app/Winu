fbbc281f086fc3e825c3f8a33fd813d37c403af6
subhash131 <subhashnayak131@gmail.com>
2024-11-20 22:24:30 +0530

    update component: MyClan

--- frontend/src/components/clan/my-clan.tsx
+++ frontend/src/components/clan/my-clan.tsx
@@ -19,34 +19,88 @@ const MyClan = () => {
   useEffect(() => {
     if (!data) return;
     console.log(data);
+    console.log(user, user);
   }, [data]);
   return (
     <div className="size-full py-3">
-      <div className="size-full-full flex items-center px-10">
-        <div className="size-full pb-2">
-          <Image
-            src="https://t4.ftcdn.net/jpg/07/96/70/85/240_F_796708506_mb31L6OZt5Spy6G9JYOQiTmiAyt9FZms.jpg"
-            alt="profile"
-            width={1}
-            height={1}
-            className="size-20 rounded-full object-cover border border-active"
-          />
-          <p className="font-semibold">Clan Name</p>
-          <p className="text-xs text-gray-300 w-[60%]">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime,
-            ipsa!
-          </p>
+      {isLoading && (
+        <div className="size-full flex items-center justify-center">
+          Loading..
         </div>
-        <button className="px-4 py-1 rounded-lg bg-white font-semibold text-black active:scale-95 transition-transform">
-          Join
-        </button>
-      </div>
-      <div className="w-full h-80 overflow-y-scroll pt-2 flex gap-2 flex-wrap custom-scrollbar">
-        <PlayerCard />
-        <PlayerCard />
-        <PlayerCard />
-        <PlayerCard />
-      </div>
+      )}
+      {!isLoading && !data?.clan && (
+        <div className="size-full flex items-center justify-center">
+          You have not joined any clan!
+        </div>
+      )}
+      {data?.clan && (
+        <>
+          <div className="size-full-full flex items-center px-10">
+            <div className="size-full pb-2">
+              <Image
+                src={data.clan.imageUrl}
+                alt={data.clan.name}
+                width={1}
+                height={1}
+                className="size-20 rounded-full object-contain border border-active"
+              />
+              <p className="font-semibold">{data.clan.name}</p>
+              <p className="text-xs text-gray-300 w-[60%]">
+                {data.clan.description}
+              </p>
+            </div>
+            <button className="px-4 py-1 rounded-lg bg-white font-semibold text-black active:scale-95 transition-transform">
+              Join
+            </button>
+          </div>
+          <div className="w-full h-80 overflow-y-scroll pt-2 flex gap-2 flex-wrap custom-scrollbar">
+            {data.clan.leader && (
+              <PlayerCard
+                name={data.clan.leader.username}
+                position="Leader"
+                image={data.clan.leader.imageUrl}
+                key=""
+              />
+            )}
+            {data?.clan?.coLeaders.map(
+              ({
+                imageUrl,
+                username,
+              }: {
+                username: string;
+                imageUrl: string;
+              }) => {
+                return (
+                  <PlayerCard
+                    name={username}
+                    position="Leader"
+                    image={imageUrl}
+                    key={username + "co-leader"}
+                  />
+                );
+              }
+            )}
+            {data?.clan?.members.map(
+              ({
+                imageUrl,
+                username,
+              }: {
+                username: string;
+                imageUrl: string;
+              }) => {
+                return (
+                  <PlayerCard
+                    name={username}
+                    position="Leader"
+                    image={imageUrl}
+                    key={username + "member"}
+                  />
+                );
+              }
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 };
