e2c16a349dc086417a64c49b6cb510ee927dd42e
subhash131 <subhashnayak131@gmail.com>
2024-11-24 23:32:39 +0530

    update component: list clan

--- frontend/src/components/tournament-form/list-clans.tsx
+++ frontend/src/components/tournament-form/list-clans.tsx
@@ -1,18 +1,37 @@
 "use client";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { usePathname } from "next/navigation";
-import React from "react";
+import React, { useEffect } from "react";
 import { IoClose } from "react-icons/io5";
 import { getClansByTournamentId } from "src/actions/tournament/get-clans-by-tournament-id";
 import Logo from "../ui/logo";
+import { removeClanFromTournament } from "src/actions/tournament/remove-clan";
+import { toast } from "sonner";
 
 const ListClans = () => {
   const tournamentId = usePathname().split("/")[3];
+  const qc = useQueryClient();
   const { isPending, error, data } = useQuery({
     queryKey: ["tournament-clans", tournamentId],
     queryFn: () => getClansByTournamentId(tournamentId),
     enabled: !!tournamentId,
   });
+
+  const { mutate, data: mutateData } = useMutation({
+    mutationFn: removeClanFromTournament,
+  });
+
+  const handleClick = (clanId: string) => {
+    mutate({ clanId, tournamentId });
+  };
+
+  useEffect(() => {
+    if (!mutateData) return;
+    toast.info(JSON.stringify(mutateData.message));
+    qc.invalidateQueries({
+      queryKey: ["tournament-clans", tournamentId],
+    });
+  }, [mutateData]);
 
   return (
     <div className="size-full flex flex-wrap gap-4">
@@ -20,7 +39,8 @@ const ListClans = () => {
         return (
           <div
             className="w-72 min-h-12 h-fit p-1 rounded-lg bg-[#202020] flex items-center justify-between px-3"
-            key={_id}
+            key={`tournament-clan-${_id}`}
+            data-clan={_id}
           >
             <div className="size-full flex items-center gap-4">
               <div className="size-12 flex items-center justify-center border border-active rounded-full overflow-hidden">
@@ -31,7 +51,10 @@ const ListClans = () => {
                 <p className="text-xs text-gray-400">{uniqueName}</p>
               </div>
             </div>
-            <button className="size-10 rounded-full border border-active shrink-0 flex items-center justify-center hover:bg-active transition-colors">
+            <button
+              className="size-10 rounded-full border border-active shrink-0 flex items-center justify-center hover:bg-active transition-colors"
+              onClick={() => handleClick(_id)}
+            >
               <IoClose size={24} />
             </button>
           </div>
