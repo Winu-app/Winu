beffb3fc28e21ad741901be18ffb1beaf6f47c48
subhash131 <subhashnayak131@gmail.com>
2024-11-27 03:46:39 +0530

    create page: tournamentPage

---
+++ frontend/src/app/(protected)/tournament/edit/[id]/end-tournament/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import EndTournament from "src/components/end-tournament";
+
+const EndTournamentPage = () => {
+  return <EndTournament />;
+};
+
+export default EndTournamentPage;
