ff4d96b9c2fa4b02c3d3a55cb57bf3bfd0d56317
subhash131 <subhashnayak131@gmail.com>
2024-11-28 03:29:08 +0530

    update useSearch hook

--- web/src/hooks/use-search.ts
+++ web/src/hooks/use-search.ts
@@ -1,31 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
-
-interface FetchSearchParams {
-  url: string;
-  query: string;
-}
-
-const fetchSearchResults = async <T>({
-  url,
-  query,
-}: FetchSearchParams): Promise<T> => {
-  if (!query) return [] as unknown as T;
-  const domain = process.env.DOMAIN || "http://localhost:3000/";
-  console.log("🚀 ~ domain:", domain);
-  console.log("🚀 ~ url:", url);
-
-  const response = await fetch(
-    `${domain}/${url}?q=${encodeURIComponent(query)}`
-  );
-  if (!response.ok) throw new Error("Failed to fetch");
-  return response.json();
-};
+import { searchClan } from "src/actions/clan/search-clan";
 
 // useSearch hook
 export const useSearch = <T>(url: string, debouncedQuery: string): any => {
   return useQuery({
     queryKey: ["search", url, debouncedQuery],
-    queryFn: () => fetchSearchResults<T>({ url, query: debouncedQuery }),
+    queryFn: () => searchClan<T>({ url, query: debouncedQuery }),
     enabled: !!debouncedQuery,
   });
 };
