633081acd187ced60e21fa8cd92c048e093a1970
subhash131 <subhashnayak131@gmail.com>
2025-01-15 15:08:46 +0530

    update xion provider

---
+++ web/src/constants/contract.ts
@@ -0,0 +1,4 @@
+export const STABLECOIN_ADDRESS =
+  "xion1sau2ukluw6lk0jxq4anrwv6a0c7kkvddv2qu6djvhx9jcxed6ltqewz37p";
+export const TREASURY_ADDRESS =
+  "xion1dgx7turjg0nz40hpnv2huvnk702jqq344j9qzv2gxrpqsgr49ndsluz9hx";
