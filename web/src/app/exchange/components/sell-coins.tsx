df12a95379fc23874a91583519991a4b0d1de5c8
subhash131 <subhashnayak131@gmail.com>
2025-01-17 17:51:01 +0530

    fix sell bug

--- web/src/app/exchange/components/sell-coins.tsx
+++ web/src/app/exchange/components/sell-coins.tsx
@@ -1,8 +1,74 @@
 "use client";
-import React, { useState } from "react";
+import {
+  useAbstraxionAccount,
+  useAbstraxionSigningClient,
+} from "@burnt-labs/abstraxion";
+import React, { useEffect, useState } from "react";
+import { toast } from "sonner";
+import { STABLECOIN_ADDRESS } from "src/constants/contract";
 
 const SellCoins = () => {
+  const [allExchanges, setAllExchanges] = useState<
+    { name: string; owner: string }[]
+  >([]);
   const [exchange, setExchange] = useState<string>("NA");
+  const [amount, setAmount] = useState(0);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [exchangeLoading, setExchangeLoading] = useState<boolean>(false);
+  const {
+    data: { bech32Address },
+    isConnected,
+  } = useAbstraxionAccount();
+  const { client } = useAbstraxionSigningClient();
+
+  const getAllExchanges = async () => {
+    try {
+      setExchangeLoading(true);
+      const exchanges = await client?.queryContractSmart(STABLECOIN_ADDRESS, {
+        get_all_exchanges: {},
+      });
+      console.log("Current supply:", exchanges);
+      return exchanges;
+    } catch (error) {
+      console.error("Failed to query GetSupply:", error);
+      throw error;
+    } finally {
+      setExchangeLoading(false);
+    }
+  };
+
+  async function sellCoins() {
+    try {
+      setLoading(true);
+      const res = await client?.execute(
+        bech32Address,
+        STABLECOIN_ADDRESS,
+        {
+          sell_to_exchange: {
+            amount,
+            exchange_address: exchange,
+          },
+        },
+        "auto",
+        "",
+        []
+      );
+      toast.success("Transaction successful");
+      setAmount(0);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    if (!client || !isConnected) return;
+    (async () => {
+      const exchanges = await getAllExchanges();
+      console.log(exchanges);
+      setAllExchanges(exchanges);
+    })();
+  }, [isConnected, client]);
   return (
     <div className="w-96 h-60 flex border border-[#333333] justify-between py-3 rounded-lg bg-[#1a1a1a] flex-col px-4">
       <h4 className="font-semibold text-xl py-2 text-center">
@@ -16,9 +82,16 @@ const SellCoins = () => {
           value={exchange}
           onChange={(e) => setExchange(e.target.value)}
         >
-          <option value="NA">Select Exchange</option>
-          <option value="E-Sports">E-Sports</option>
-          <option value="others">Other Exchange</option>
+          <option value="NA">
+            {exchangeLoading ? "Loading..." : "Select Exchange"}
+          </option>
+          {allExchanges.map(({ name, owner }) => (
+            <option value={owner} key={owner}>
+              {name}
+            </option>
+          ))}
+          {!isConnected && <option value="NA">Connect Wallet</option>}
+          {isConnected && <option value="others">Other Exchange</option>}
         </select>
       </div>
       {exchange === "others" && (
@@ -26,6 +99,7 @@ const SellCoins = () => {
           <input
             placeholder="exchange address"
             className="bg-transparent outline-none px-3 py-1 border-b border-[#333333]"
+            onChange={(e) => setExchange(e.target.value)}
           />
         </div>
       )}
@@ -34,10 +108,14 @@ const SellCoins = () => {
           placeholder="enter amount"
           className="bg-transparent outline-none px-3 py-1 border-b border-[#333333]"
           type="number"
+          onChange={(e) => setAmount(Number(e.target.value))}
         />
       </div>
-      <button className="px-3 py-1 bg-white rounded-md text-black font-semibold">
-        Sell
+      <button
+        className="px-3 py-1 bg-white rounded-md text-black font-semibold"
+        onClick={sellCoins}
+      >
+        {loading ? "loading..." : "Sell"}
       </button>
     </div>
   );
